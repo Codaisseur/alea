5b014c04cf2c004ffb05fb54d9ccfa0e7ba7d26d
Wouter de Vos <wouter@codaisseur.com>
2016-07-22 11:12:44 +0200

    Render plain, text is deprecated apparently

--- app/controllers/postgres_databases_controller.rb
+++ app/controllers/postgres_databases_controller.rb
@@ -1,6 +1,6 @@
 class PostgresDatabasesController < ApplicationController
   def create
     pg = PostgresDatabase.create
-    render text: "DATABASE_URL=#{pg.database_url}"
+    render plain: "DATABASE_URL=#{pg.database_url}"
   end
 end
